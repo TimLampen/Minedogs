12b52245781408d57bbe3689b03cc169ca2c6f11
TimLampen <minetendodevelopers@gmail.com>
2014-05-24 11:00:41 -0700

    Added Commands

--- src/com/minetendo/minesurvival/MineSurvival.java
+++
@@ -1,8 +0,0 @@
-package com.minetendo.minesurvival;
-
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class MineSurvival extends JavaPlugin implements Listener{
-
-}
