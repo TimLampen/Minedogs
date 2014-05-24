0511fb41983c2d5bff10261a40ccfcc897a1ce57
TimLampen <lampentimothy42@gmail.com>
2014-05-24 10:01:33 -0700

    First Commit

---
+++ src/com/minetendo/minesurvival/MineSurvival.java
@@ -0,0 +1,8 @@
+package com.minetendo.minesurvival;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineSurvival extends JavaPlugin implements Listener{
+
+}
